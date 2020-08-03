26045484c80396660be70e96efb67b523246c44d
peawarrior <jacksonchen171@gmail.com>
2020-08-02 20:41:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    let answer = [];
+    for (let i = 0; i < names.length; i++) {
+        answer.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return answer;
+}
+
+function countDown(number) {
+    let countup = 0
+    while (countup <= number) {
+        console.log(countup)
+        countup++
+    }
+}
